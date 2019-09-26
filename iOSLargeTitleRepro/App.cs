d39f03ca9c6986f9c91d96a1b33fb08812b2cee1
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-09-26 10:33:31 -0700

    First Commit

---
+++ iOSLargeTitleRepro/App.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace iOSLargeTitleRepro
+{
+    public class App : Xamarin.Forms.Application
+    {
+        public App()
+        {
+            var contentPage = new ContentPage
+            {
+                Title = "Wrong Toolbar Background Color",
+                Content = new Xamarin.Forms.ScrollView
+                {
+                    Content = new Label
+                    {
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.Center,
+                        Text = "Main Page"
+                    }
+                }
+            };
+
+            var navigationPage = new Xamarin.Forms.NavigationPage(contentPage)
+            {                
+                BarBackgroundColor = Color.Green,
+                BarTextColor = Color.Red
+            };
+
+            navigationPage.On<iOS>().SetPrefersLargeTitles(true);
+
+            MainPage = navigationPage;
+        }
+    }
+}
